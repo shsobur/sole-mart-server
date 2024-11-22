db7c2dcfb561b41e8992f7a43e473ee820616fbf
shsobur <shsoburhossen951@gmail.com>
2024-11-22 07:54:30 -0600

    Added patch mathod

--- index.js
+++ index.js
@@ -115,6 +115,19 @@ async function run() {
       res.send(result);
     })
 
+    // Make admin__ __ __!
+    app.patch("/make-seller/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          status: "approved"
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })    
+
 
 
 
