d52474d6ea82f811ef49964ca26df01e9966282d
shsobur <shsoburhossen951@gmail.com>
2024-11-22 09:42:33 -0600

    Code last push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 port = process.env.PORT || 8000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -9,10 +11,28 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(
   cors({
     origin: "http://localhost:5173",
+    credentials: true,
     optionsSuccessStatus: 200,
   })
 );
 app.use(express.json());
+app.use(cookieParser());
+
+// Verify token__ __!
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.accessToken;
+  if (!token) {
+    return res.status(401).send({ massage: "Unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ massage: "Unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4yea9q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -29,9 +49,32 @@ async function run() {
     const productCollection = client.db("solemart").collection("products");
     const userCollection = client.db("solemart").collection("users");
 
+    // JWT__ __!
 
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "10h",
+      });
+      res
+        .cookie("accessToken", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
 
-    // Post signup users__ __ __!
+    // Logout__ __!
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      res.clearCookie("accessToken", { maxAge: 0 }).send({ success: true });
+    });
+
+    // Post signup users__ __!
+
     app.post("/user", async (req, res) => {
       const user = req.body;
       console.log(user);
@@ -39,15 +82,15 @@ async function run() {
       res.send(result);
     });
 
-    // Get all user__ __ __!
-    app.get("/users", async (req, res) => {
+    // Get all user__ __!
+
+    app.get("/users", verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-    })
+    });
 
+    // Get user data__ __!
 
-
-    // Get user data__ __ __!
     app.get("/user-data/:email", async (req, res) => {
       const user = req.params.email;
       const query = { userEmail: user };
@@ -55,9 +98,8 @@ async function run() {
       res.send(result);
     });
 
+    // Fing all category__ __!
 
-
-    // Fing all category__ __ __!
     app.get("/all-categorys", async (req, res) => {
       const result = await productCollection
         .aggregate([
@@ -68,16 +110,16 @@ async function run() {
       res.send(result);
     });
 
+    // Post new product__ __!
 
-    // Post new product__ __ __!
     app.post("/add-product", async (req, res) => {
       const item = req.body;
-      const result = await productCollection.insertOne(item)
+      const result = await productCollection.insertOne(item);
       res.send(result);
-    })
+    });
 
+    // Get all products__ __!
 
-    // Get all products__ __ __!
     app.get("/all-products", async (req, res) => {
       const { name, category, sort } = req.query;
       const query = {};
@@ -99,37 +141,37 @@ async function run() {
       res.json(result);
     });
 
-    // Get seller product__ __ __!
-    app.get("/seller-product/:email", async (req, res) => {
+    // Get seller product__ __!
+
+    app.get("/seller-product/:email", verifyToken, async (req, res) => {
       const sellerEmail = req.params.email;
-      const query = {sellerEmail: sellerEmail};
+      const query = { sellerEmail: sellerEmail };
       const result = await productCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
-    // Delete product__ __ __!
-    app.delete("/product-delete/:id", async (req, res) => {
+    // Delete product__ __!
+
+    app.delete("/product-delete/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await productCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-    // Make admin__ __ __!
-    app.patch("/make-seller/:id", async (req, res) => {
+    // Seller req approved__ __!
+
+    app.patch("/make-seller/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          status: "approved"
-        }
-      }
+          status: "approved",
+        },
+      };
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })    
-
-
-
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
